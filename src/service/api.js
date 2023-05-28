62100fe07344f5a7701c343b21849630a6de6701
Aman-210 <119395304+Aman-210@users.noreply.github.com>
2023-05-28 11:45:21 +0530

    weather_app

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '16f78aaded705efa72743f8071153408';
-const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export const getWeather = async (City, Country) => {
     try {
